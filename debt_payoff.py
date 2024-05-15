0048df1911b00fa4a2b1633f5af248bfca4d54e8
Gary Hines <ghines1001@gmail.com>
2024-05-15 15:53:16 -0500

    Added payoff date logic

--- debt_payoff.py
+++ debt_payoff.py
@@ -3,12 +3,25 @@
 # to calculate the interest portion balance * (interest / 12) 
 # subtract the interest portion from the payment
 # then subtract the payment from the balance
+
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 debt_name = "my big fat debt"
 min_payment = 50.00
 interest = .12
 balance = 500.00
 month = 0
-extra_pay = 50.00
+extra_pay = 00.00
+starting_month = "Jun"
+starting_year = 2024
+
+def compute_payoff_month(starting_month, starting_year, month):
+    initial_date_string = starting_month + " 1, " + str(starting_year)
+    initial_date = datetime.strptime(initial_date_string, "%b %d, %Y")
+    payoff_date = initial_date + relativedelta(months=+(month-1))
+    return datetime.strftime(payoff_date, "%b %Y")
+
 
 while balance > 0:
     month += 1
@@ -20,4 +33,7 @@ while balance > 0:
     balance -= payment
     print(f"Month {month}: You paid ${payment:.2f} towards {debt_name}. Balance left is ${balance:.2f} Interest was: {interest_portion:.2f}") 
 
-print("All done")
+
+# Need to use month-1 below as the month is increased before the payment. 
+print(f"Payoff date is {compute_payoff_month(starting_month, starting_year, (month-1))}")
+
